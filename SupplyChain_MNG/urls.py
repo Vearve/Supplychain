fb3b6dc4f707f25521add21a39babed69d558f1c
MansLikeNight <evabloo2342@gmail.com>
2026-04-23 13:45:01 +0200

    fix: add favicon route and static icon

--- SupplyChain_MNG/urls.py
+++ SupplyChain_MNG/urls.py
@@ -21,8 +21,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.svg', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('SupplyChain_MNG.SupplChain_MNG.urls')),
 ]
